bac0464b7572dc458de69a259ef1fd5c770c1f6c
ssse0y <ko527ko@gmail.com>
2022-11-15 16:42:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+void swap(int *x,int *y){
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;}
 	
-	int i=300; 
-	
-	int *pi=&i;
-	char *pc=&i;
-	
-	printf("%i, %i, %i\n", i, *pi, *pc);
-	
+	void main(void){
+		int a=3;
+		int b=5;
+		swap(&a,&b);		
+		printf("a:%i, b:%i\n",a, b);
 	return 0;
 }
